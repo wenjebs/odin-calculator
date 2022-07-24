275b4f01b4350524d63e06e0613a0643fec06559
wenjie <wenjieb2@gmail.com>
2022-07-24 21:57:40 +0800

    Create sum,divide,multiply,subtract fxns

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 function addNum(num1, num2) {
-
+    return num1 + num2;
 };
 
 function subtractNum(num1, num2) {
-
+    return num1 - num2;
 };
 
 function multiplyNum(num1, num2) {
-
+    return num1 * num2;
 };
 
 function divideNum(num1, num2) {
-
+    return num1 / num2;
 };

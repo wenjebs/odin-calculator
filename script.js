07f4014c8e1160b8807b96123213b15a57165b77
wenjie <wenjieb2@gmail.com>
2022-07-24 22:17:08 +0800

    Add operate function with switch

--- script.js
+++ script.js
@@ -13,3 +13,16 @@ function multiplyNum(num1, num2) {
 function divideNum(num1, num2) {
     return num1 / num2;
 };
+
+function operate(operator, num1, num2) {
+    switch(operator) {
+        case '+':
+            return addNum(num1,num2);
+        case '-':
+            return subtractNum(num1,num2);
+        case '/':
+            return divideNum(num1,num2);
+        case '*':
+            return multiplyNum(num1,num2);
+    }
+};

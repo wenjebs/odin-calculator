d5c44c6a527560557d25b7b900d9183b6957887e
wenjie <wenjieb2@gmail.com>
2022-07-25 01:13:36 +0800

    Fix up clear and equal buttons

--- script.js
+++ script.js
@@ -4,34 +4,50 @@ const clear = document.querySelector(".clear");
 const numbers = document.querySelectorAll(".number");
 const operations = document.querySelectorAll(".operation");
 let input = '';
-
+let storeResult = '';
+let operator;
 //listen for mouse click on equal button
 equal.addEventListener("click", function() {
     display.textContent = '';
-    let result = (operate(`${operator}`, parseInt(num), parseInt(input)));
-    display.textContent = result;
-    
+    // if there hasnt been a previous result use the one u inputted
+    if (!storeResult) {
+        let result = (operate(`${operator}`, parseInt(num), parseInt(input)));
+        display.textContent = result;
+        storeResult = result;
+    // else if there is a result alr from previous operations u shd use it
+    } else {
+        let result = (operate(`${operator}`, parseInt(storeResult), parseInt(input)));
+        display.textContent = result;
+        //update new storedresult
+        storeResult = result;
+    }
 });
 
 //listen for clear
 clear.addEventListener("click", function() {
     display.textContent = '';
-    input = 0;
-    num = 0;
+    input = '';
+    num = '';
+    storeResult = '';
 });
 
 //for each input number
 numbers.forEach(number => number.addEventListener("click", function() {
     appendToDisplay(number.textContent);
+    // store ur input
     input+=number.textContent;
 }));
 
 //for each operation + - / *
 operations.forEach(operation => operation.addEventListener("click", function() {
-    operator = operation.textContent;
+    // when u press + - / * save the previous input
     num = input;
+    // delete the current saved input
     input = '';
+    // add to display
+    operator = operation.textContent;
     appendToDisplay(` ${operator} `);
+    return operator;
 }));
 
 //add the numbers onto the display
